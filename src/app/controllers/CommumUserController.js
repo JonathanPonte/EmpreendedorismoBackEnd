aeb5cbdee19158f7c8f08e8fcf98ae72750d60d6
JonathanPonte <jonathancardoso2@hotmail.com>
2020-04-27 13:51:56 -0300

    add media rota resposta da escala

--- src/app/controllers/CommumUserController.js
+++ src/app/controllers/CommumUserController.js
@@ -15,30 +15,42 @@ const People = require('../models/People');
 async function answerScale(req, res) {
     const { peopleId, scaleId, answers } = req.body;
 
-    
-
     try {
         const people = await People.findById(peopleId);
         const scale = await Scale.findById(scaleId);
-    
+
+        await AnswersOfPeople.deleteOne({ scale, people });
+
         const answersOfPeople = new AnswersOfPeople();
         answersOfPeople.people = people;
         answersOfPeople.scale = scale;
         const answersP = []
+        var respPeople = 0;
+        var respTotal = scale.maxScaleValue * scale.questions.length;
+        var correctScore = scale.correctScores;
+
         await Promise.all(answers.map(async a => {
             const eachAnswer = new Answer()
             eachAnswer.result = a.result
             eachAnswer.question = a.question
+            respPeople = respPeople + a.result;
             await eachAnswer.save()
             answersP.push(eachAnswer);
         }))
         answersOfPeople.answers = answersP;
         await answersOfPeople.save(); 
-        
 
-        //gerar media;
+        const valueBefore = 10/correctScore;
+        const valueAfter = 10/((respTotal + 1) - correctScore);
+        var media = 0;
 
-        return res.send({ answersOfPeople });
+        if(respPeople <= correctScore){
+            media = respPeople * valueBefore;
+        }else{
+            media = 10 - ((respPeople - correctScore) * valueAfter);
+        }
+
+        return res.send({ media: media });
     } catch (error) {
         console.log(error);
         return res.status(400).send({ error: 'Error answer scale' });
